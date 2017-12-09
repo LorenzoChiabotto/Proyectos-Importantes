ed30587b27fb035ac1470407a73b6cda01b6a7ba
LorenzoChiabotto <34394898+LorenzoChiabotto@users.noreply.github.com>
2017-12-09 09:28:51 -0300

    TestTecnico MAvha

---
+++ TestTecnico/WebForms/AltaModificacion.aspx.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Data.SqlClient
+
+Public Class AltaModificacion
+    Inherits System.Web.UI.Page
+
+    Dim conexion As SqlConnection = New SqlConnection(ConfigurationManager.AppSettings("strconexion"))
+    Dim consulta As SqlCommand
+    Dim reader As SqlDataReader
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If (Not IsPostBack()) Then
+            If (Request.QueryString("accion") = "EDIT") Then
+                Try
+                    consulta = New SqlCommand("Select * from Personas WHERE id_Persona =@id", conexion)
+                    consulta.Parameters.Add("@id", SqlDbType.Int)
+                    consulta.Parameters("@id").Value = Request.QueryString("id")
+
+                    conexion.Open()
+                    reader = consulta.ExecuteReader()
+                    reader.Read()
+
+                    tbNombre.Text = reader("nombre_Persona").ToString
+                    tbNacimiento.Text = reader("nacimiento_Persona").ToString
+                    tbEdad.Text = reader("edad_Persona").ToString
+
+
+                    ddSexo.SelectedValue = reader("sexo_Persona").ToString
+
+
+                    reader.Close()
+
+                    conexion.Close()
+                Catch ex As Exception
+
+                End Try
+
+            End If
+        End If
+    End Sub
+
+    Protected Sub btGuardar_Click(sender As Object, e As EventArgs) Handles btGuardar.Click
+        If (Request.QueryString("accion") = "EDIT") Then
+            Try
+                consulta = New SqlCommand("UPDATE Personas SET nombre_Persona = @nombre, nacimiento_Persona = @nacimiento, edad_Persona = @edad, sexo_Persona = @sexo where id_Persona=@id", conexion)
+
+
+                consulta.Parameters.Add("@id", SqlDbType.Int)
+                consulta.Parameters("@id").Value = Request.QueryString("id")
+
+                consulta.Parameters.Add("@nombre", SqlDbType.VarChar)
+                consulta.Parameters("@nombre").Value = tbNombre.Text
+
+                consulta.Parameters.Add("@nacimiento", SqlDbType.VarChar)
+                consulta.Parameters("@nacimiento").Value = tbNacimiento.Text
+
+                consulta.Parameters.Add("@edad", SqlDbType.Int)
+                consulta.Parameters("@edad").Value = tbEdad.Text
+
+                consulta.Parameters.Add("@sexo", SqlDbType.VarChar)
+                consulta.Parameters("@sexo").Value = ddSexo.SelectedValue.ToString()
+
+
+                conexion.Open()
+
+                consulta.ExecuteNonQuery()
+
+                conexion.Close()
+            Catch ex As Exception
+
+            End Try
+
+        Else
+            Try
+                consulta = New SqlCommand("Insert into Personas(nombre_Persona, nacimiento_Persona, edad_Persona, sexo_Persona, baja_Persona) Values  (@nombre, @nacimiento, @edad, @sexo,0)", conexion)
+
+                consulta.Parameters.Add("@nombre", SqlDbType.VarChar)
+                consulta.Parameters("@nombre").Value = tbNombre.Text
+
+                consulta.Parameters.Add("@nacimiento", SqlDbType.VarChar)
+                consulta.Parameters("@nacimiento").Value = tbNacimiento.Text
+
+                consulta.Parameters.Add("@sexo", SqlDbType.VarChar)
+                consulta.Parameters("@sexo").Value = ddSexo.SelectedValue.ToString()
+
+                consulta.Parameters.Add("@edad", SqlDbType.Int)
+                consulta.Parameters("@edad").Value = tbEdad.Text
+
+                conexion.Open()
+
+                consulta.ExecuteNonQuery()
+
+                conexion.Close()
+            Catch ex As Exception
+
+            End Try
+
+
+        End If
+
+        Response.Redirect("ListadoPersonas.aspx?")
+    End Sub
+End Class
---
+++ TestTecnico/WebForms/Eliminacion.aspx.designer.vb
@@ -0,0 +1,60 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Eliminacion
+    
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+    
+    '''<summary>
+    '''Control lblTabla.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblTabla As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control lblRegistro.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblRegistro As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control btAceptar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btAceptar As Global.System.Web.UI.WebControls.Button
+    
+    '''<summary>
+    '''Control btCancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btCancelar As Global.System.Web.UI.WebControls.Button
+End Class
